31ace6d62da6918c6fb24da784a4f6acff759c8d
MUGABA <mugabamuha@gmail.com>
2019-03-07 15:19:10 +0300

    This is on users

--- api/Users/validators.py
+++ api/Users/validators.py
@@ -2,6 +2,7 @@ from flask import jsonify, request,json,abort
 from . models import User
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from re import search
 
 from . methods import UserList
 
@@ -26,18 +27,18 @@ def create_new_user():
 
 
 
-	id = userlist.generate_user_id()
+	user_id = userlist.generate_user_id()
 
 	isadmin = False
 	registered = date_today = datetime.now().strftime('%d%m%y %H%M')
 
-	new_user  = User(id, firstname,lastname,othername,username,email, \
+	new_user  = User(user_id, firstname,lastname,othername,username,email, \
 		password,phonenumber,registered,isadmin)
 
 	if email == '' or username == '' or password == '':
 		return jsonify({"message" : "email, username and password cannot be empty"}),200
 
-	password = generate_password_hash(password)
+	new_user.password = generate_password_hash(password)
 
 	if new_user.email:
 		for usie in userlist.user_list:
@@ -46,9 +47,14 @@ def create_new_user():
 	if new_user.username:
 		for usie in userlist.user_list:
 			if new_user.username == usie['username']:
-				return jsonify({"message" : "email already taken"}),200
+				return jsonify({"message" : "username already taken"}),200
 	userlist.add_a_user(new_user),201
-	return jsonify({"user" : userlist.user_list[-1]}),200
+
+	curr_user = userlist.user_list[-1]
+	return jsonify({"user" : {
+		"username": curr_user['username']
+
+		} }),201
 
 
 def getall_users():
@@ -57,20 +63,39 @@ def getall_users():
 def sign_in_user():
 	data = request.get_json()
 
-	usernme = data.get('usernme')
+	usernme = data.get('username')
+	print(usernme)
+	passcode = data.get('password')
+	print(passcode)
 
-	passcode = data.get('passcode')
 
 	if usernme == '' or passcode == '':
 		return jsonify({"message" : "password or username can not be empty"}),400
-
 	if not type(usernme) == str:
-		return jsonify({"message" : "username must be a whole string"}),400
+		return jsonify({"message": "useranme must be a string"})
+	usernme = (usernme).strip()
 
 	if not type(passcode) == str:
-		return jsonify({"message" : "password must be a string"}),400
+		return jsonify({"message": "useranme must be a string"})
+	passcode = (passcode).strip()
+	#passcode = generate_password_hash(passcode)
 	for user in userlist.user_list:
+		print(user['username'])
+		print(usernme)
+		print(user['password'])
+		print(passcode)
 		if user['username'] == usernme and check_password_hash(user['password'],passcode):
+
+			print(user['username'])
+			print(usernme)
+			print(passcode)
+			
 			return jsonify({"message" : "your welcome {} your loged in ".format(usernme)}),200
-	return jsonify({"message" : "The user doesnot exist"})
+		#print(userlist.user_list)
+		else:
+			return jsonify({"message" : "The user doesnot exist"}),201
+
+
+def index_page():
+	return jsonify({"message": "Your welcome to ireporter"}),200
 
--- api/Users/views.py
+++ api/Users/views.py
@@ -1,18 +1,19 @@
 from flask import Blueprint
-from . validators import create_new_user,getall_users,sign_in_user
+from . validators import create_new_user,getall_users,sign_in_user,index_page
 
 
 userblueprint = Blueprint('api',__name__)
 
+@userblueprint.route('/')
+def home():
+	return index_page()
+
 @userblueprint.route('/users', methods = ['POST'] )
 def create_user():
-
 	return create_new_user()
-
 @userblueprint.route('/users',methods = ['GET'] )
 def fetch_all_users():
 	return getall_users()
-
 @userblueprint.route('/login',methods = ['POST'])
 def login_user():
 	return sign_in_user()
